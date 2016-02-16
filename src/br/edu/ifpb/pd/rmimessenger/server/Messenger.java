9164d23f709ce5f319114c99e435792fd9db23ac
joaopaulo11jp <joaopaulo11jp@gmail.com>
2016-02-16 00:02:05 -0300

    Some fixes...

--- src/br/edu/ifpb/pd/rmimessenger/server/Messenger.java
+++ src/br/edu/ifpb/pd/rmimessenger/server/Messenger.java
@@ -25,17 +25,10 @@ public class Messenger extends UnicastRemoteObject implements MessengerIF{
 	@Override
 	public boolean joinMessenger(ClientIF client) throws RemoteException {
 		if(this.clients.get(client.getName()) == null){
-			Iterator<ClientIF> it;
-			Collection<String> list = new ArrayList<String>();
 			
 			this.clients.put(client.getName(), client);
 			System.out.println(client.getName()+" entrou no chat!");
-			
-			it = this.clients.values().iterator();
-			list.addAll(this.clients.keySet());
-			while(it.hasNext()){
-				it.next().setUserList(list);
-			}
+			refreshUsers();
 			
 			return true;
 		}else{
@@ -50,7 +43,7 @@ public class Messenger extends UnicastRemoteObject implements MessengerIF{
 		
 		while(it.hasNext()){
 			client = (ClientIF) it.next();
-			client.message(fromUser+" : "+msg+" "+new SimpleDateFormat("dd/MM/yyyy HH:mm").format(new Date())+"\n");
+			client.message(fromUser+" : "+msg+" ["+new SimpleDateFormat("dd/MM/yyyy HH:mm").format(new Date())+"]\n");
 		}
 	}
 
@@ -60,8 +53,8 @@ public class Messenger extends UnicastRemoteObject implements MessengerIF{
 		if(user.equals(fromUser)){
 			return -1;
 		}else if(this.clients.get(user) != null){
-			this.clients.get(user).message(fromUser+" (PVT recebido): "+msg+" "+new SimpleDateFormat("dd/MM/yyyy HH:mm").format(new Date())+"\n");
-			this.clients.get(fromUser).message(fromUser+" (PVT enviado para "+user+"): "+msg+" "+new SimpleDateFormat("dd/MM/yyyy HH:mm").format(new Date())+"\n");
+			this.clients.get(user).message(fromUser+" (PVT recebido): "+msg+" ["+new SimpleDateFormat("dd/MM/yyyy HH:mm").format(new Date())+"]\n");
+			this.clients.get(fromUser).message(fromUser+" (PVT enviado para "+user+"): "+msg+" ["+new SimpleDateFormat("dd/MM/yyyy HH:mm").format(new Date())+"]\n");
 			return 1;
 		}else return 0;
 	}
@@ -70,6 +63,18 @@ public class Messenger extends UnicastRemoteObject implements MessengerIF{
 	public void exitMessenger(ClientIF client) throws RemoteException {
 		System.out.println(client.getName()+" saiu do chat!");
 		this.clients.remove(client.getName());
+		refreshUsers();
+	}
+	
+	public void refreshUsers() throws RemoteException{
+		Iterator<ClientIF> it;
+		Collection<String> list = new ArrayList<String>();
+		it = this.clients.values().iterator();
+		
+		list.addAll(this.clients.keySet());
+		while(it.hasNext()){
+			it.next().setUserList(list);
+		}
 	}
 	
 }
